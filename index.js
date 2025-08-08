5229559703f493b3feb10950a89ce4e305f0ce8e
Chicano0 <urielbalderas957@gmail.com>
2025-08-07 19:33:30 -0600

    Index.js Funcionando

--- index.js
+++ index.js
@@ -50,6 +50,9 @@ app.get('/api/user', (req, res) => {
 });
 
 // Iniciar servidor
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Servidor corriendo en http://192.168.1.14:3000');
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+
+app.listen(PORT, HOST, () => {
+  console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
 });
